8265cbfcbef83047c0acb1dda7d7b8cf193663f5
Fábio Vicente <fab10_lima@hotmail.com>
2021-11-24 17:07:01 -0300

    Requisito 9: Implementando o botão limpar

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const pixels = 5;
 const colorPalette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
 
+//  PALETA DE CORES
 for (let i = 0; i < colors; i += 1) {
   colorPalette.appendChild(document.createElement('div'));
   colorPalette.children[i].className = 'color';
@@ -19,6 +20,9 @@ function select(click) {
 }
 
 colorPalette.addEventListener('click', select);
+
+//  QUADRO DE PIXELS
+const clear = document.getElementById('clear-board');
 
 for (let i = 0; i < pixels; i += 1) {
   pixelBoard.appendChild(document.createElement('div'));
@@ -37,5 +41,18 @@ function paintPixel(click) {
     clicked.target.style.backgroundColor = bgClrClicked;
   }
 }
+function clearPainting() {
+/*   for (const line of pixelBoard.children) {
+    for (const pixel of line.children) {
+      pixel.style.backgroundColor = 'white';
+    }
+  } */
+  for (let i = 0; i < pixelBoard.children.length; i += 1) {
+    for (let j = 0; j < pixelBoard.children[i].children.length; j += 1) {
+      pixelBoard.children[i].children[j].style.backgroundColor = 'white';
+    }
+  }
+}
 
 pixelBoard.addEventListener('click', paintPixel);
+clear.addEventListener('click', clearPainting);
